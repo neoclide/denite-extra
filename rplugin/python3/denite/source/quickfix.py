79f5aaa9e96a1e7441c77227c46ef49b48fefce4
Dane Summers <dsummersl@yahoo.com>
2017-11-01 22:21:11 -0400

    Adds default action 'cc', and modifies builtin quickfix action.

--- rplugin/python3/denite/source/quickfix.py
+++ rplugin/python3/denite/source/quickfix.py
@@ -6,6 +6,8 @@
 # pylint: disable=E0401,C0411
 import re
 from .base import Base
+from ..kind.file import Kind as FileKind
+
 
 class Source(Base):
 
@@ -16,6 +18,8 @@ class Source(Base):
         self.kind = 'file'
         self.matchers = ['matcher_fuzzy']
         self.sorters = []
+
+        self.kind = Kind(vim)
 
     def define_syntax(self):
         self.vim.command('syntax case ignore')
@@ -35,7 +39,7 @@ class Source(Base):
         self.vim.command('highlight default link deniteSource_QuickfixPosition LineNr')
 
 
-    def convert(self, val, context):
+    def convert(self, val, index, context):
         bufnr = val['bufnr']
         line = val['lnum'] if bufnr != 0 else 0
         col = val['col'] if bufnr != 0 else 0
@@ -47,17 +51,36 @@ class Source(Base):
 
         return {
             'word': word,
+            'action__text': val['text'],
             'action__path': fname,
             'action__line': line,
             'action__col': col,
             'action__buffer_nr': bufnr,
+            'action__index': index,
             }
 
     def gather_candidates(self, context):
         items = self.vim.eval('getqflist()')
         res = []
 
-        for item in items:
+        for idx, item in enumerate(items):
             if item['valid'] != 0:
-                res.append(self.convert(item, context))
+                res.append(self.convert(item, idx + 1, context))
         return res
+
+
+class Kind(FileKind):
+    """ Support the 'cc' quickfix action, storing where you are in the quickfix
+    list, so that you can take advantage of :cnext/:cprev features.
+    """
+
+    def __init__(self, vim):
+        super().__init__(vim)
+        self.default_action = 'cc'
+        self.name = 'commands'
+        self.persist_actions = []
+
+    def action_cc(self, context):
+        target = context['targets'][0]
+        index = target['action__index']
+        self.vim.command('cc {}'.format(index))
